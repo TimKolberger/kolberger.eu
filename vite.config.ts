f6a4d8936815637c50d5fde7f6de658ee7722516
Tim Kolberger <tim@kolberger.eu>
2024-03-08 21:05:57 +0100

    ci: add vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { vercelPreset } from "@vercel/remix/vite";
 
+const maybeVercelPresets = process.env.VERCEL ? [vercelPreset()] : [];
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [
+    remix({
+      presets: maybeVercelPresets,
+    }),
+  ],
 });
